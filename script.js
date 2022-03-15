2a1af218dc8a5ba8767b0333c6226ea35d978c36
Smooke09 <pedrolucas1014zz@gmail.com>
2022-03-15 12:17:13 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function Converter() {
+    let opcao = document.getElementById('moedas');
+    let moeda = opcao.options[opcao.selectedIndex].text;
+    console.log(moeda)
+    let valor = document.getElementById('valor').value;
+    console.log(valor)
+
+
+
+    if (moeda === 'Dolar') {
+        let dolarValor = 5.14
+        let resultadoDolar = valor / dolarValor
+        let convert = document.getElementById('valorConvertido')
+        convert.innerHTML = `Seu valor em dolar e ${resultadoDolar.toFixed(2)} USD`
+        console.log(resultadoDolar)
+    } else if (moeda === 'Euro') {
+        let euroValor = 5.65
+        let resultadoEuro = valor / euroValor
+        let convert = document.getElementById('valorConvertido')
+        convert.innerHTML = `Seu valor em Euro e ${resultadoEuro.toFixed(2)} EUR`
+    } else if (moeda === 'Remimbi') {
+        let remimbiValor = 0.81
+        let resultadoRemimbi = valor / remimbiValor
+        let convert = document.getElementById('valorConvertido')
+        convert.innerHTML = `Seu valor em Euro e ${resultadoRemimbi.toFixed(2)}`
+    }
+}
